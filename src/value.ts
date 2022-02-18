8c36753ef22c308d7be19d717e3f3001cc2fce93
linyyyang <linyyyang@tencent.com>
2022-02-18 15:25:41 +0800

    fix: 修复配置获取无效的bug

--- src/value.ts
+++ src/value.ts
@@ -1,19 +1,12 @@
 /*
- * @Description: 
- * @Usage: 
- * @Author: richen
- * @Date: 2022-02-18 10:57:24
- * @LastEditTime: 2022-02-18 11:18:46
- */
-/*
  * @Author: richen
  * @Date: 2020-12-18 10:37:03
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-02-15 13:49:31
+ * @LastEditTime: 2022-02-18 15:24:19
  * @License: BSD (3-Clause)
  * @Copyright (c) - <richenlin(at)gmail.com>
  */
-import { IOCContainer } from "koatty_container";
+import { IOCContainer, TAGGED_ARGS } from "koatty_container";
 
 /**
  * Indicates that an decorated configuration as a property.
@@ -23,30 +16,29 @@ import { IOCContainer } from "koatty_container";
  * @param {string} [type] configuration type
  * @returns {PropertyDecorator}
  */
-export function Value(key?: string, type?: string): PropertyDecorator {
+export function Config(key?: string, type?: string): PropertyDecorator {
     return (target: any, propertyKey: string) => {
-        // identifier = identifier || helper.camelCase(propertyKey, { pascalCase: true });
-        key = key || propertyKey;
-        type = type || "config";
-        // IOCContainer.savePropertyData(TAGGED_ARGS, `${key ?? ""}|${type || "config"}`, target, propertyKey);
         const app = IOCContainer.getApp();
         if (!app || !app.config) {
             return;
         }
-        Reflect.defineProperty(target.prototype, propertyKey, {
-            enumerable: true,
-            configurable: false,
-            writable: true,
-            value: app.config(key, type)
-        });
+        // identifier = identifier || helper.camelCase(propertyKey, { pascalCase: true });
+        key = key || propertyKey;
+        type = type || "config";
+        IOCContainer.savePropertyData(TAGGED_ARGS, {
+            name: propertyKey,
+            method: function () {
+                return app.config(key, type);
+            }
+        }, target, propertyKey);
     };
 }
 /**
  * Indicates that an decorated configuration as a property.
  *
- * @export
+ * @deprecated use `Config` instead
  * @param {string} identifier configuration key
  * @param {string} [type] configuration type
  * @returns {PropertyDecorator}
  */
-export const Config = Value;
+export const Value = Config;
